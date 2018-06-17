25e68fe8ea11d856a34c9a53fbb123f3c3fb42f2
Esat GÖZCÜ <35576161+esatgozcu@users.noreply.github.com>
2018-06-17 19:47:10 +0300

    Add files via upload

---
+++ ServoMotorKullanimi.ino
@@ -0,0 +1,22 @@
+#include <Servo.h>                // Servo kütüphanesini dahil ediyoruz
+
+Servo motor;                      // Bir örnek oluşturuyoruz.
+
+void setup() {
+
+  motor.attach(6);                // 9. pwm pinini ayarlıyoruz.
+}
+
+void loop() {
+
+  for(int aci=0; aci<=180; aci++){
+
+    motor.write(aci);
+    delay(10);
+  }
+  for(int aci=180; aci>=0; aci--){
+
+    motor.write(aci);
+    delay(10);
+  }
+}
